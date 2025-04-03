69c37762054a46a16e7637ca0756abf60b2606fd
Zed <skhull.coding@proton.me>
2025-04-03 13:20:26 +0530

    feat: from the prev login

--- src/providers/AdminTokenProvider.jsx
+++ src/providers/AdminTokenProvider.jsx
@@ -1,9 +1,28 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { checkAdminUrl } from "../../constants/server";
 
 const AdminTokenContext = createContext();
 
 const AuthTokenProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState(null);
+
+  const checkAndApplyUserToken = async (authToken) => {
+    const response = await fetch(checkAdminUrl, {
+      method: "GET",
+      headers: { auth_token: authToken },
+    });
+
+    const result = await response.json();
+    if (result.success === true) setAuthToken(authToken);
+  };
+
+  useEffect(() => {
+    if (Cookies.get("auth_token") !== null)
+      checkAndApplyUserToken(Cookies.get("auth_token"));
+
+    return () => {};
+  }, []);
 
   return (
     <AdminTokenContext.Provider value={{ token: authToken, set: setAuthToken }}>
