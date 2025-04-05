ae38977bd7d6a2ef156e9cc35c43dd36e4faad17
Zed <skhull.coding@proton.me>
2025-04-06 03:36:20 +0530

    feat: design for login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Input } from "@/components/ui/input"
-
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 const LoginForm = () => {
@@ -11,11 +18,26 @@ const LoginForm = () => {
   return (
     <form
       onSubmit={handleLogin}
-      className="w-48 flex flex-col items-center gap-4"
+      className="flex flex-col items-center gap-4"
     >
-      <Input type="username" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <Button className="cursor-pointer">Submit &rarr;</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Admin Account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2 w-64">
+            <Input type="username" placeholder="Username" />
+            <Input type="password" placeholder="Password" />
+            <Button className="cursor-pointer mt-3" type="submit">
+              Submit &rarr;
+            </Button>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <p>&copy; Masked Redhat 2025</p>
+        </CardFooter>
+      </Card>
     </form>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,11 @@ import LoginForm from "../components/LoginForm";
 const Login = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center flex-col gap-6">
-      <h1 className="text-5xl font-semibold">Admin Panel - Login</h1>
+      <h1 className="text-4xl font-semibold flex flex-col gap-0.5 text-center">
+            <span>ThoughtsHub</span>
+            <span className="text-xl font-light tracking-wider text-gray-600">Admin Panel</span>
+
+      </h1>
       <LoginForm />
     </div>
   );
