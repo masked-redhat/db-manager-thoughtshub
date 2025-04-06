80de8277859c32a6f6cd888d3880494e9519f37a
Zed <skhull.coding@proton.me>
2025-04-06 15:11:08 +0530

    style: news card

--- src/components/PanelHome.jsx
+++ src/components/PanelHome.jsx
@@ -3,6 +3,14 @@ import { requestAuth } from "../../utils/request";
 import { getNewsUrl } from "../../constants/server";
 import { useToken } from "../providers/AdminTokenProvider";
 import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const PanelHome = () => {
   const [latestNews, setLatestNews] = useState([]);
@@ -48,40 +56,61 @@ const PanelHomeSection = ({ title, data }) => {
         <h1 className="text-2xl font-bold">{title}</h1>
       </header>
 
-      <div className="flex flex-col gap-2.5">
-        {data.map((news) => {
-          return (<div className="w-full h-64 border border-gray-300 rounded-xl overflow-hidden shadow-md flex flex-col md:flex-row p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-            {/* Image */}
-            {news.imageUrl ? (
-              <img
-                src={news.imageUrl}
-                alt="Error"
-                className="w-full md:w-64 h-40 md:h-full object-cover rounded-md mb-4 md:mb-0 md:mr-4 bg-gray-200 text-gray-500"
-              />
-            ) : (
-              <div className="w-full md:w-64 h-40 md:h-full bg-gray-200 flex items-center justify-center text-gray-500 text-sm rounded-md mb-4 md:mb-0 md:mr-4">
-                No Image
-              </div>
-            )}
-      
-            {/* Content */}
-            <div className="flex-1 flex flex-col gap-2 overflow-auto">
-              <div className="text-xs text-gray-500 break-all">ID: {news.id}</div>
-              <div className="text-lg font-semibold">{news.title}</div>
-              <div className="text-sm">{news.body}</div>
-              <div className="text-sm text-gray-600 break-all">News URL: {news.newsUrl || 'N/A'}</div>
-              <div className="text-sm">Category: <span className="font-medium">{news.category}</span></div>
-              <div className="text-sm text-gray-500">
-                Created: {new Date(Number(news.createDate)).toLocaleString()}
-              </div>
-              <div className="text-sm text-gray-500">
-                Updated: {new Date(Number(news.updateDate)).toLocaleString()}
-              </div>
-            </div>
-          </div>)
-        })}
+      <div className="flex flex-col gap-4">
+        {data.map((news) => (
+          <NewsCard news={news} />
+        ))}
       </div>
     </section>
+  );
+};
+
+const NewsCard = ({ news }) => {
+  return (
+    <div className="w-full lg:h-64 border border-gray-300 rounded-xl overflow-hidden shadow-md flex flex-col lg:flex-row p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      {/* Image */}
+      {news.imageUrl ? (
+        <img
+          src={news.imageUrl}
+          alt="Error"
+          className="w-full lg:w-64 h-64 lg:h-full object-cover rounded-md mb-4 lg:mb-0 lg:mr-4 bg-gray-200 text-gray-500"
+        />
+      ) : (
+        <div className="w-full lg:w-64 h-40 lg:h-full bg-gray-200 flex items-center justify-center text-gray-500 text-sm rounded-md mb-4 lg:mb-0 lg:mr-4">
+          No Image
+        </div>
+      )}
+
+      {/* Content */}
+      <div className="flex-1 flex flex-col gap-2 overflow-auto">
+        <div className="text-xs text-gray-500 break-all">
+          {news.id.toUpperCase()}
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="text-2xl font-semibold">{news.title} </div>
+          <Badge>{news.category}</Badge>
+        </div>
+        <div className="flex text-xs gap-3">
+          <div className="text-gray-500">
+            {new Date(Number(news.createDate)).toLocaleString()}
+          </div>
+          {news.createDate !== news.updateDate && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>( Edited )</TooltipTrigger>
+                <TooltipContent>
+                  <div>
+                    {new Date(Number(news.updateDate)).toLocaleString()}
+                  </div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+        </div>
+
+        <div className="text-sm">{news.body}</div>
+      </div>
+    </div>
   );
 };
 
