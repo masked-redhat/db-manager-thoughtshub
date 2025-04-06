7b041cdf68adb44b0e649642f7914fe60381764e
Zed <skhull.coding@proton.me>
2025-04-06 17:39:29 +0530

    feat: upload news form

--- constants/server.js
+++ constants/server.js
@@ -6,3 +6,4 @@ export const newsUploadUrl = `${apiUrl}/admin/news`;
 export const logoutUrl = `${apiUrl}/logout`;
 export const uploadUrl = `${apiUrl}/upload`;
 export const getNewsUrl = `${apiUrl}/news`
+export const getCategoriesUrl = `${apiUrl}/categories`
--- src/components/news/PanelCreateNews.jsx
+++ src/components/news/PanelCreateNews.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { uploadAuth } from "../../../utils/request";
-import { uploadUrl } from "../../../constants/server";
+import { requestAuth, uploadAuth } from "../../../utils/request";
+import {
+  getCategoriesUrl,
+  newsUploadUrl,
+  uploadUrl,
+} from "../../../constants/server";
 import { Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,12 +15,60 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useToken } from "../../providers/AdminTokenProvider";
+import { toast, Toaster } from "sonner";
 
 const PanelCreateNews = () => {
-  const [file, setFile] = useState("");
+  const { token } = useToken();
 
-  const handleSubmit = (e) => {
+  const [file, setFile] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [category, setCategory] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [newsUrl, setNewsUrl] = useState("");
+
+  const [newsUploading, setNewsUploading] = useState(false);
+
+  const resetForm = () => {
+    setFile("");
+    setCategory("");
+    setImageUrl("");
+    setTitle("");
+    setBody("");
+    setNewsUrl("");
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setNewsUploading(true);
+
+    const data = { title, body, imageUrl, category, newsUrl };
+
+    const response = await requestAuth(newsUploadUrl, "POST", token, data);
+
+    const result = await response.json();
+    if (response.ok) {
+      toast("News uploaded", {
+        description: `News created with title ${title}`,
+        action: {
+          label: "Log",
+          onClick: () => console.log(result),
+        },
+      });
+      resetForm();
+    }
+
+    setNewsUploading(false);
+    resetForm();
   };
 
   return (
@@ -25,16 +77,85 @@ const PanelCreateNews = () => {
         <h1 className="text-3xl font-extrabold mb-1">Create News</h1>
         <hr />
       </header>
-      <form onSubmit={handleSubmit} className="w-full px-2">
-        <NewsImageUploader file={file} setFile={setFile} />
-        <Input type="text" placeholder="Title" />
-        <Textarea placeholder="Body" />
+      <form onSubmit={handleSubmit} className="w-full px-2 flex flex-col gap-3">
+        <NewsImageUploader
+          file={file}
+          setFile={setFile}
+          setImageUrl={setImageUrl}
+        />
+        <div className="flex flex-col gap-3 w-full max-w-[540px]">
+          <Input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <Textarea
+            placeholder="Body"
+            className="max-h-56"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            required
+          />
+          <SelectCategory category={category} setCategory={setCategory} />
+          <Input
+            type="text"
+            placeholder="News Url"
+            value={newsUrl}
+            onChange={(e) => setNewsUrl(e.target.value)}
+            required
+          />
+          <Button className="w-fit" disabled={newsUploading}>
+            {newsUploading ? (
+              <>
+                <Loader2 className="animate-spin" />
+                <span>Please wait</span>
+              </>
+            ) : (
+              <p>Submit &rarr;</p>
+            )}
+          </Button>
+        </div>
+        <Toaster />
       </form>
     </section>
   );
 };
 
-const NewsImageUploader = ({ file, setFile }) => {
+const SelectCategory = ({ category, setCategory }) => {
+  const [categories, setCategories] = useState([]);
+  const fetchCategoriesAndSet = async () => {
+    const response = await fetch(getCategoriesUrl);
+
+    const result = await response.json();
+    if (response.ok) setCategories(result.categories);
+  };
+
+  useEffect(() => {
+    fetchCategoriesAndSet();
+    return () => {};
+  }, []);
+
+  return (
+    <Select value={category} onValueChange={setCategory}>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.map((c) => {
+          return (
+            <SelectItem key={c} value={c}>
+              {c}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
+};
+
+const NewsImageUploader = ({ file, setFile, setImageUrl }) => {
   const [uploadingFile, setUploadingFile] = useState(false);
   const [uploadedFile, setUploadedFile] = useState("");
 
@@ -47,7 +168,10 @@ const NewsImageUploader = ({ file, setFile }) => {
 
     const result = await response.json();
 
-    if (response.ok) setUploadedFile(result.fileUrl);
+    if (response.ok) {
+      setUploadedFile(result.fileUrl);
+      setImageUrl(result.fileUrl);
+    }
 
     setUploadingFile(false);
   };
@@ -59,14 +183,15 @@ const NewsImageUploader = ({ file, setFile }) => {
   };
 
   useEffect(() => {
-    uploadFileAndShow();
+    if (file) uploadFileAndShow();
+    else setUploadedFile("");
 
     return () => {};
   }, [file]);
 
   return (
-    <div>
-      <div className="w-96 h-56 bg-black rounded-md relative flex items-center justify-center text-white group">
+    <div className="flex flex-col gap-1 items-center w-fit">
+      <div className="w-96 h-56 bg-black rounded-md relative flex  items-center justify-center text-white group">
         {uploadedFile.length !== 0 ? (
           <>
             <img
@@ -76,7 +201,7 @@ const NewsImageUploader = ({ file, setFile }) => {
             />
             <Button
               variant="outline"
-              className="absolute right-0 bottom-0 mx-3 my-3 bg-black/75 border-gray-500 cursor-pointer text-white shadow-sm  opacity-0 group-hover:opacity-100"
+              className="absolute right-0 bottom-0 mx-3 my-3 bg-black/75 cursor-pointer text-white shadow-sm opacity-0 border-0 group-hover:opacity-100"
               onClick={removeImage}
             >
               Remove
@@ -94,7 +219,10 @@ const NewsImageUploader = ({ file, setFile }) => {
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
-              <p className="max-w-96 overflow-hidden truncate whitespace-nowrap">
+              <p
+                className="max-w-96 overflow-hidden truncate whitespace-nowrap
+              "
+              >
                 <span className="font-bold">Image:</span> {file.name}
               </p>
             </TooltipTrigger>
