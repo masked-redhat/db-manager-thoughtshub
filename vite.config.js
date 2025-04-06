fa881245b203457408e28f0cb8f9c12499df8c86
Zed <skhull.coding@proton.me>
2025-04-06 06:17:30 +0530

    fix: env vars were not loaded

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,21 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: `../${process.env.backend}/public/dist`,
-    emptyOutDir: true, // also necessary
-  },
-  base: "/dist",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: `../${process.env.backend}/public/dist`,
+    emptyOutDir: true, // also necessary
+  },
+  base: "/dist",
 });
