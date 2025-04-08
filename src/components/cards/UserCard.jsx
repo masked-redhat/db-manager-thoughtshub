37a2aa6d101ee594900ab3907347104452ece909
Zed <skhull.coding@proton.me>
2025-04-08 21:29:09 +0530

    feat: check users

--- src/components/cards/UserCard.jsx
+++ src/components/cards/UserCard.jsx
@@ -1,46 +1,154 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import WriterCard from "./WriterCard";
+import { getUsersUrl } from "../../../constants/server";
+import { toast } from "sonner";
+import { useToken } from "../../providers/AdminTokenProvider";
+import { Badge } from "@/components/ui/badge";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
 const UserCard = ({ data, fetchUsers }) => {
+  const { token } = useToken();
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="lg:w-lg w-full border border-gray-300 rounded-xl shadow-md flex flex-col p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-white gap-3 relative">
-      {console.log(data)}
+    <div className="w-96 border border-gray-300 rounded-xl shadow-md flex flex-col p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-white gap-3 relative">
+      <div className="flex flex-col gap-0.5 text-gray-500">
+        <span className="text-xs uppercase font-medium"> {data.id}</span>
+        <span className="text-xs uppercase font-medium">
+          {data.Profile?.id ?? "N/A"}
+        </span>
+      </div>
 
-      {/* Image */}
-      {data.Profile.profileImageUrl ? (
-        <img
-          src={data.Profile.profileImageUrl}
-          alt="Error"
-          className="w-full h-64 object-contain rounded-md lg:mr-4 bg-black text-gray-500"
+      <div className="flex items-center justify-between">
+        <WriterCard
+          data={
+            data.Profile ?? {
+              ...data,
+              username: data.username ?? data.email ?? data.mobile ?? "N/A",
+              fullName: data.username ?? data.email ?? data.mobile ?? "N/A",
+            }
+          }
         />
-      ) : (
-        <div className="w-full h-32 bg-gray-200 flex items-center justify-center text-gray-500 rounded-md lg:mr-4 text-5xl capitalize">
-          {data.Profile.fullName}
+        <ActionBtn userId={data.id} fetchUsers={fetchUsers} token={token} />
+      </div>
+
+      <div className="flex flex-wrap gap-2 items-center">
+        <Badge variant="secondary">
+          {data.Profile === null ? "Not Profiled" : "Profiled"}
+        </Badge>
+      </div>
+
+      <div className="flex flex-col">
+        <span className="font-medium">
+          <span className="font-light">Email : </span>
+          {typeof data.email === "string" ? data.email : "N/A"}
+        </span>
+        <span className="font-medium">
+          <span className="font-light">Mobile : </span>{" "}
+          {typeof data.mobile === "string" ? data.mobile : "N/A"}
+        </span>
+      </div>
+
+      <div>
+        <span className="flex items-center gap-2 font-light">
+          Password :
+          <Button
+            variant="secondary"
+            className="flex gap-1 items-center cursor-pointer font-medium"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? (
+              data.password
+            ) : (
+              <>
+                <span className="w-1 h-1 rounded-full bg-black"></span>
+                <span className="w-1 h-1 rounded-full bg-black"></span>
+                <span className="w-1 h-1 rounded-full bg-black"></span>
+              </>
+            )}
+          </Button>
+        </span>
+      </div>
+
+      <div className="flex flex-col gap-0.5">
+        <span className="font-medium">
+          <span className="font-light">Forums Uploaded : </span>
+          {data.Profile?.forumsCount ?? "N/A"}
+        </span>
+        <span className="font-medium">
+          <span className="font-light">Gender : </span>
+          {data.Profile !== null
+            ? typeof data.Profile?.gender === "string"
+              ? data.Profile?.gender
+              : "N/A"
+            : "N/A"}
+        </span>
+      </div>
+
+      <div className="flex text-xs gap-3">
+        <div className="text-gray-500">
+          {new Date(Number(data.createDate)).toLocaleString()}
         </div>
-      )}
-
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
-            <span className="uppercase text-xs">User Id: {data.id}</span>
-          </TooltipTrigger>
-          <TooltipContent>
-            <span className="uppercase">Profile Id: {data.Profile.id}</span>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-
-      <h2 className="text-xl font-black">{data.Profile.fullName}</h2>
-      <p className="flex items-center gap-1 -mt-3.5">
-        <span className="font-bold">@</span>
-        {data.username}
-      </p>
+        {data.createDate !== data.updateDate && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>( Edited )</TooltipTrigger>
+              <TooltipContent>
+                <div>{new Date(Number(data.updateDate)).toLocaleString()}</div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+      </div>
     </div>
+  );
+};
+
+const ActionBtn = ({ userId, token, fetchUsers }) => {
+  const [deleting, setDeleting] = useState(false);
+
+  const handleDeleteUser = async () => {
+    setDeleting(true);
+
+    const response = await requestAuth(getUsersUrl, "DELETE", token);
+
+    if (response.ok) {
+      toast("User Deleted");
+      fetchUsers();
+    } else toast("User couldn't be deleted");
+
+    setDeleting(false);
+  };
+
+  return (
+    <DropdownMenu className="rounded-full">
+      <DropdownMenuTrigger className="flex items-center justify-center p-1 rounded-full cursor-pointer">
+        <HiOutlineDotsVertical />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem disabled={true}>Edit</DropdownMenuItem>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          disabled={deleting}
+          onClick={handleDeleteUser}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
